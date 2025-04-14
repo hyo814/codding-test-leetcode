7a4df8e08bf8bbedcda24a0c498113d56d2b4b18
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2025-04-14 21:31:02 +0900

    Time: 14 ms (91.8%), Space: 61.5 MB (82.92%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -0,0 +1,15 @@
+function findRepeatedDnaSequences(s) {
+  const seen = new Set();
+  const repeated = new Set();
+
+  for (let i = 0; i <= s.length - 10; i++) {
+    const sequence = s.substring(i, i + 10);
+    if (seen.has(sequence)) {
+      repeated.add(sequence);
+    } else {
+      seen.add(sequence);
+    }
+  }
+
+  return Array.from(repeated);
+}
